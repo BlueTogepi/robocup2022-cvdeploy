a1c6581325a03e8cb539ce03d0fda8f31def9480
Pon-ek Tangmunchittham <pon-ek@hotmail.com>
2022-01-31 06:57:28 +0700

    Add image sending, time benchmarking

--- person_tracking/server.py
+++ person_tracking/server.py
@@ -3,11 +3,39 @@ import socket
 import os
 import cv2
 import numpy as np
+import struct
+import time
 
 print("Server started")
 
 HOST = "0.0.0.0"
 PORT = int(os.environ.get('SOCKET_PORT'))
+CAM_WIDTH = int(os.environ.get('CAM_WIDTH'))
+CAM_HEIGHT = int(os.environ.get('CAM_HEIGHT'))
+
+def send_msg(sock, msg):
+    # Prefix each message with a 4-byte length (network byte order)
+    msg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(msg)
+
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return recvall(sock, msglen)
+
+def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
@@ -15,17 +43,11 @@ s.listen()
 print("Listening at port %d..." % PORT)
 while True:
     conn, addr = s.accept()
-    data = b""
     print('Connected by', addr)
-    while True:
-        pkt = conn.recv(1024)
-        data += pkt
-        print(addr, ":", repr(pkt))
-        if not data:
-            print(addr, "has finished sending")
-            break
-    # img = np.frombuffer(data, dtype=np.uint8)
-    # img = cv2.resize(img, (504, 896))
-    # cv2.imwrite("test_img/received.jpg", img)
-    print("Received:", repr(data))
-    s.send(b"Ack from server")
+    start = time.time()
+    data = recv_msg(conn)
+    img = np.frombuffer(data, dtype=np.uint8)
+    img = np.reshape(img, (CAM_WIDTH, CAM_HEIGHT, 3))
+    cv2.imwrite("test_img/received.jpg", img)
+    end = time.time()
+    conn.send(str(end - start).encode())
--- socket_test_py3.py
+++ socket_test_py3.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python3
 import socket
 import cv2
+import struct
 
 HOST = 'localhost'
 PORT_PERSONTRACK = 4501
 PORT_WHATSTHIS = 4502
 
-# img = cv2.imread("test_img/PC_Webcam_M_1.jpg")
+def send_msg(sock, msg):
+    # Prefix each message with a 4-byte length (network byte order)
+    msg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(msg)
+
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return recvall(sock, msglen)
+
+def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
+
+img = cv2.imread("test_img/PC_Webcam_M_1.jpg")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT_PERSONTRACK))
-    # s.sendall(img.tobytes())
-    s.sendall(b"abcde")
+    send_msg(s, img.tobytes())
     data = s.recv(1024)
 
-print('Received', repr(data))
+print('Received, Time =', data.decode())
